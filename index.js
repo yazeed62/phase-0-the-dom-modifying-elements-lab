a1bc8beeda0dd4bc58e39072a9c52310f00a179e
yazeedahmed <dee3ayahmed25@gmail.com>
2023-03-23 01:32:10 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+const mainElement = document.querySelector('#main');
+mainElement.remove();
+
+const newHeader = document.createElement ('h1');
+const newHeaderTittle = document.createElement('victory');
+newHeaderTittle.textContent = 'victory';
+
+newHeader.id = "victory";
+newHeader.textContent = "YAZEED AHMED is the champion "; document.bodyappend(newHeader);
+
+
+
+
+
+
+
+
+
+
+
+
+
